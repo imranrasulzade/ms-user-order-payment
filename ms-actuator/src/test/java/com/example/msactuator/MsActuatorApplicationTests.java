003e39563883e7f28181b63a07a8c47033ebcfdc
imranrasulzade <imranrasulzade5@gmail.com>
2025-06-11 01:36:16 +0400

    actuator service add demo

---
+++ ms-actuator/src/test/java/com/example/msactuator/MsActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
